44364938e206b61e08200f9668d14a871d1cc272
Wesleyjjx <Sharpenerhd@hotmail.com>
2023-10-09 14:55:12 -0300

    ajustes
    lendo arquivo txt

--- main.cpp
+++ main.cpp
@@ -7,25 +7,25 @@ using namespace std;
 int main()
 {
     int l, c;
-    ofstream arq;
-    arq.open("dadosGrafo.txt");
-    arq << "4 0 1 1 0 1 0 1 0 1 1 0 1 0 0 1 0" << endl;
-    arq.close();
-    ifstream arqA;
+    fstream arq;
+    arq.open("dadosGrafo1.txt",ios::in);
+    //arq << "10 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1 0 1 0 1 1 0 1 0 0 1" << endl;
+    //arq.close();
+    //ifstream arqA;
     string linha;
-    arqA.open("dadosGrafo.txt");
-    arqA >> l;
+    //arqA.open("dadosGrafo1.txt");
+    arq >> l;
     c = l;
     int m[l][c];
     for (int i = 0; i < l; i++)
     {
         for (int j = 0; j < c; j++)
         {
-            arqA >> m[i][j];
+            arq >> m[i][j];
         }
         cout << endl;
     }
-    arqA.close();
+    arq.close();
     for (int i = 0; i < l; i++)
     {
         for (int j = 0; j < c; j++)
@@ -34,16 +34,16 @@ int main()
         }
         cout << endl;
     }
-    ofstream arqB;
-    arqB.open("matrizDeAdjacencias.txt");
+    ofstream arqA;
+    arqA.open("matrizDeAdjacencias.txt");
     for (int i = 0; i < l; i++)
     {
         for (int j = 0; j < c; j++)
         {
-            arqB << m[i][j] << " - ";
+            arqA << m[i][j] << " - ";
         }
-        arqB << "\n";
+        arqA << "\n";
     }
-    arqB.close();
+    arqA.close();
     return 0;
 }
